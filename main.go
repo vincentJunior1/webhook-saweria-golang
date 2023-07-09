627493c775157f3a0467a3e0e66443b483c3c9a3
vincent junior <liekian71@gmail.com>
2023-07-09 17:56:52 +0700

    add open browser feature

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os/exec"
 	"runtime"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -18,6 +21,35 @@ type WebhookReq struct {
 	Cut          int    `json:"cut"`
 	DonatorName  string `json:"donator_name"`
 	DonatorEmail string `json:"donator_email"`
+	Message      string `json:"message"`
+}
+
+var ContainNameAkil = map[string]bool{
+	"akil":   true,
+	"yummin": true,
+	"yummy":  true,
+	"yami":   true,
+}
+
+var Ambatukam = map[string]bool{
+	"ambatukam":  true,
+	"ambadeblou": true,
+	"ambasing":   true,
+	"ambatunat":  true,
+}
+
+var EnoBening = map[string]bool{
+	"eno":    true,
+	"bening": true,
+}
+
+var Dhika = map[string]bool{
+	"dhika":      true,
+	"dika":       true,
+	"ceo":        true,
+	"kalinsun":   true,
+	"cleansound": true,
+	"andhikanug": true,
 }
 
 func main() {
@@ -38,6 +70,7 @@ func main() {
 			time.Sleep(2 * time.Second)
 		}
 
+		var howManyTimesWwillClicked int = 10
 		time.Sleep(5 * time.Second)
 		fmt.Println("Ini ammoutnya", payload.AmountRaw)
 		switch payload.AmountRaw {
@@ -51,6 +84,7 @@ func main() {
 		case 50000:
 			kb.HasALT(true)
 			kb.SetKeys(keybd_event.VK_F4)
+			howManyTimesWwillClicked = 4
 		case 100000:
 			kb.SetKeys(139)
 		case 1000000:
@@ -69,12 +103,54 @@ func main() {
 		}
 
 		// Or you can use Press and Release
-		for i := 0; i < 10; i++ {
+		for i := 0; i < howManyTimesWwillClicked; i++ {
 			kb.Press()
 		}
 		time.Sleep(3 * time.Second)
 		kb.Release()
 		kb.Clear()
+
+		go func() {
+			var err error
+			var url string
+			var contain bool
+			messageArray := strings.Split(payload.Message, " ")
+
+			for _, val := range messageArray {
+				tmp := strings.ToLower(val)
+				if _, contain := ContainNameAkil[tmp]; contain {
+					url = "https://www.youtube.com/watch?v=84qzw26k5Oc&t=7s"
+					contain = true
+					break
+				}
+
+				if _, ambatukam := Ambatukam[tmp]; ambatukam {
+					url = "https://www.youtube.com/watch?v=rV5ynCW-kVw"
+					contain = true
+				}
+
+				if _, eno := EnoBening[tmp]; eno {
+					url = "https://www.youtube.com/watch?v=4zD-3lxMivo&t=27s"
+					contain = true
+					break
+				}
+
+				if _, dhika := Dhika[tmp]; dhika {
+					url = "https://www.youtube.com/watch?v=JiIHhAOjytw"
+					contain = true
+					break
+				}
+			}
+
+			if contain {
+				err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+
+			}
+
+			if err != nil {
+				log.Fatal(err)
+			}
+		}()
 
 		ctx.JSON(http.StatusOK, gin.H{
 			"message": "pong",
